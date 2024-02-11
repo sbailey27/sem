ad76397f3083c5a3a99d889d3068091744679b92
sbailey27 <40679456@live.napier.ac.uk>
2024-02-11 14:57:04 -0400

    new java classes, connect and disconnect (lab 3)

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+// App.java
+public class App {
+    public void connect() {
+        // Implement connection logic here
+        System.out.println("Connected to database");
+    }
+
+    public void disconnect() {
+        // Implement disconnection logic here
+        System.out.println("Disconnected from database");
+    }
+}
