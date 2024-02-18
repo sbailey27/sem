bf44f7547a7d4f376667a4b8cd16efa2d9ab3b99
sbailey27 <40679456@live.napier.ac.uk>
2024-02-18 09:25:38 -0400

    the app finally works properly...for now

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-// Main.java
-public class Main {
-    public static void main(String[] args) {
-        App app = new App(); // Create an object of the App class
-        app.connect(); // Call connect method
-        app.disconnect(); // Call disconnect method
-    }
-}
